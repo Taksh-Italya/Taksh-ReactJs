eb3dbd2958ae61425d57afcb2a96e201e251740a
Taksh-Italiya <italiyataksh088@gmail.com>
2025-02-21 10:50:11 +0530

    21-2-25

---
+++ index.js
@@ -0,0 +1,29 @@
+// const array1 = [10,20,30]
+
+// console.log(array1);
+
+// const users = {
+//     name : "Taksh",
+//     email :"takshitaliya133@gmail.com",
+//     age : 21,
+//     gender :"male",
+//     contect:9000080000,
+//     fun : function(){
+//         console.log("hello i am ", `${this.name}`);
+//     }
+// }
+
+// console.log(users);
+
+const arr1 =[100,200,300];
+// const arr2 =[400,500,600];
+
+// const marge = [...arr1 ,...arr2];
+// arr1.push(400);
+// arr1.pop()
+// arr1.shift()
+arr1.unshift()
+console.log(arr1);
+
+
+
